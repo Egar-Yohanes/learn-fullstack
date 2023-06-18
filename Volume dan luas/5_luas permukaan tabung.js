b93bcb1ce13c606b90ffe8052be299d459904686
Egar-Yohanes <yohanesbaptista149@gmail.com>
2023-06-18 14:12:04 +0700

    luas dan volume

---
+++ Volume dan luas/5_luas permukaan tabung.js
@@ -0,0 +1,21 @@
+/* Algoritma luas permukaan tabung = 
+1. Buat Variabel phi
+2. Buat variabel r
+3. Buat variabel t
+4. Buat variabel Luas
+5. Tampilkan Variabel Luas
+*/
+
+/* Pseudocode Luas Pemrukaan Tabung
+1. ISI variabel phi DENGAN angka 3.14
+2. ISI variabel r DENGAN angka 4
+3. ISI variabel t DENGAN angka 8
+4. ISI variabel Luas dengan 2*phi*r (r+t) 
+*/
+
+// Coding
+let phi = 3.14;
+let r =  4;
+let t = 8;
+let Luas = 2*phi*r*(r+t);
+console.log(Luas);
