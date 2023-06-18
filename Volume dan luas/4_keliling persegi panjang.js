4a510e34457386262981c8413a615ea521c4eed8
Egar-Yohanes <yohanesbaptista149@gmail.com>
2023-06-18 13:54:23 +0700

    Volume dan luas

---
+++ Volume dan luas/4_keliling persegi panjang.js
@@ -0,0 +1,25 @@
+// console.log("Hello World");
+// study case
+
+
+
+/* buatlah algoritma dan pseudocode dari keliling persegi panjang
+Buat Variabel panjang 
+Buat Variabel lebar 
+Buat Variabel Keliling 
+Tampilkan hasil
+*/
+
+/* pseudocode :
+ISI panjang DENGAN angka 2
+ISI lebar DENGAN angka 4
+ISI Keliling DENGAN 2 * (panjang + lebar)
+Tampilkan Keliling
+*/
+
+
+/*Coding*/
+let panjang = 10 ;
+let lebar = 5;
+let keliling = 2 * (panjang + lebar);
+console.log(keliling);
