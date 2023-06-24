8bedadbaaadc7d7b530dcb9fd6f25279ffce8518
Egar-Yohanes <yohanesbaptista149@gmail.com>
2023-06-24 09:35:20 +0700

    Perulangan

---
+++ Perulangan/for.js
@@ -0,0 +1,5 @@
+// for = perulangan dengan batasan
+for(var j = 1; j <=10; j++) {
+    console.log(j);
+}
+
---
+++ Perulangan/while.js
@@ -0,0 +1,5 @@
+// perulangan bisa dengan/ tanpa batasan
+let i = 1;
+while(i<=10) {
+    console.log(i);i++;
+}
